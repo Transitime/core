cd5d33ee668b7b42dbdb66ebdab7ec5b48b4d3a2
Skibu Smith <skibu@smith.net>
2014-05-19 18:48:37 -0700

    Added distanceAlongStopPath to toString() method.

--- mainProject/src/main/java/org/transitime/core/TemporalMatch.java
+++ mainProject/src/main/java/org/transitime/core/TemporalMatch.java
@@ -55,6 +55,7 @@ public class TemporalMatch extends SpatialMatch {
 				+ ", isLayover=" + isLayover()
 				+ ", distanceToSegment=" + Geo.distanceFormat(distanceToSegment) 
 				+ ", distanceAlongSegment=" + Geo.distanceFormat(distanceAlongSegment)
+				+ ", distanceAlongStopPath=" + Geo.distanceFormat(getDistanceAlongStopPath())
 				+ ", atStop=" + atStop
 				+ ", trip=" + getTrip().toShortString()
 				+ "]";
