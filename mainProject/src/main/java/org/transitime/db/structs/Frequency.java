6489e2140383a8695d2facab7b8a8efbcd596220
Skibu Smith <skibu@smith.net>
2014-06-04 11:22:26 -0700

    Fixed comments. No functional changes.

--- mainProject/src/main/java/org/transitime/db/structs/Frequency.java
+++ mainProject/src/main/java/org/transitime/db/structs/Frequency.java
@@ -221,6 +221,24 @@ public class Frequency implements Serializable {
 	}
 
 	/**
+	 * The exact_times field determines if frequency-based trips should be
+	 * exactly scheduled based on the specified headway information. Valid
+	 * values for this field are:
+	 * 
+	 * <ul>
+	 * <li>0 or (empty) - Frequency-based trips are not exactly scheduled. This
+	 * is the default behavior. 1 - Frequency-based trips are exactly scheduled.
+	 * For a frequencies.txt row, trips are scheduled starting with
+	 * trip_start_time = start_time + x * headway_secs for all x in (0, 1, 2,
+	 * ...) where trip_start_time < end_time.</li>
+	 * <li>The value of exact_times must be the same for all frequencies.txt
+	 * rows with the same trip_id. If exact_times is 1 and a frequencies.txt row
+	 * has a start_time equal to end_time, no trip must be scheduled. When
+	 * exact_times is 1, care must be taken to choose an end_time value that is
+	 * greater than the last desired trip start time but less than the last
+	 * desired trip start time + headway_secs.</li>
+	 * </ul>
+	 * 
 	 * @return the exactTimes
 	 */
 	public boolean getExactTimes() {
