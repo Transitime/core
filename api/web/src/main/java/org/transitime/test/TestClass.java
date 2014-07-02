fb16fda12ba2d1eaf3edc08b6a9fa59985435444
Skibu Smith <skibu@smith.net>
2014-07-01 22:32:40 -0700

    Testing pushing class from new web project to GitHub.

---
+++ api/web/src/main/java/org/transitime/test/TestClass.java
@@ -0,0 +1,39 @@
+/*
+ * This file is part of Transitime.org
+ * 
+ * Transitime.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL) as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Transitime.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Transitime.org .  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.transitime.test;
+
+/**
+ * Just for testing working with Eclipse Dynamic web project and multiple
+ * GitHub repos.
+ *
+ * @author SkiBu Smith
+ *
+ */
+public class TestClass {
+
+     /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+    }
+
+    /********************** Member Functions **************************/
+
+}
