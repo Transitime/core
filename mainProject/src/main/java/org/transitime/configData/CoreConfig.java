6067a3ca1c27afeaa1bab4a9ac6704a46bf9fc91
Skibu Smith <skibu@smith.net>
2014-08-18 18:45:36 -0700

    Changed RMI ports to use CoreConfig so they are settable and to have default values of 2099 and 3099.

--- mainProject/src/main/java/org/transitime/configData/CoreConfig.java
+++ mainProject/src/main/java/org/transitime/configData/CoreConfig.java
@@ -136,6 +136,42 @@ public class CoreConfig {
 					+ "the web database, speeding up testing.");
 	
 	/**
+	 * Which port to use for RMI calls. Usually RMI uses port 1099 but using
+	 * default of 2099 to not interfere with other RMI based applications
+	 * 
+	 * @return
+	 */
+	public static int rmiPort() {
+		return rmiPort.getValue();
+	}
+	private static IntegerConfigValue rmiPort =
+			new IntegerConfigValue("transitime.core.rmiPort",
+					2099,
+					"Which port to use for RMI calls. Usually RMI uses port "
+					+ "1099 but using default of 2099 to not interfere with "
+					+ "other RMI based applications.");
+
+	/**
+	 * Which secondary port to use for RMI calls, for once initial communication
+	 * has been established. Usually RMI uses port 0 which means any port. But
+	 * then can't configure firewall to limit access to specific ports.
+	 * Therefore using default value of 3099 so that the port is consistent.
+	 * 
+	 * @return
+	 */
+	public static int secondaryRmiPort() {
+		return secondaryRmiPort.getValue();
+	}
+	private static IntegerConfigValue secondaryRmiPort =
+			new IntegerConfigValue("transitime.core.secondaryRmiPort",
+					2099,
+					"Which secondary port to use for RMI calls, for once "
+					+ "initial communication has been established. Usually "
+					+ "RMI uses port 0 which means any port. But then can't "
+					+ "configure firewall to limit access to specific ports. "
+					+ "Therefore using default value of 3099 so that the port " 
+					+ "is consistent.");
+	/**
 	 * When batching large amount of AVL data through system to generate
 	 * improved schedule time (as has been done for Zhengzhou) it takes huge
 	 * amount of time to process everything. To speed things up you can set
--- mainProject/src/main/java/org/transitime/ipc/rmi/RmiParams.java
+++ mainProject/src/main/java/org/transitime/ipc/rmi/RmiParams.java
@@ -16,20 +16,37 @@
  */
 package org.transitime.ipc.rmi;
 
+import org.transitime.configData.CoreConfig;
+
 /**
- *
+ * For defining port numbers to be used for RMI
+ * 
  * @author SkiBu Smith
  *
  */
 public class RmiParams {
-	// Use the standard port 1099 but can change that here if necessary
-	public static int rmiPort = 2099;
+	// Usually will use special port 2099 in case another app is using 
+	// standard RMI port 1099
+	public static int rmiPort = CoreConfig.rmiPort();
+	
+	// For secondary communication. Usually RMI uses port 0, which means any old
+	// random port, but to deal with firewalls it is better to use a fixed port.
+	public static int secondaryRmiPort = CoreConfig.secondaryRmiPort();
 	
 	public static int getRmiPort() { 
 		return rmiPort;
 	}
-	
+
 	public static void setRmiPort(int newRmiPort) {
 		rmiPort = newRmiPort;
 	}
+
+	public static int getSecondaryRmiPort() {
+		return secondaryRmiPort;
+	}
+	
+	public static void setSecondaryRmiPort(int newSecondaryRmiPort) {
+		secondaryRmiPort = newSecondaryRmiPort;
+	}
+	
 }
