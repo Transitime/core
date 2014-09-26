27eb72b6f149028d1033e93875dbd476b4e9f1f5
Skibu Smith <skibu@smith.net>
2014-09-25 17:41:26 -0700

    Many new features. Appears to be really useful now for visualizing and finding issues.

--- mainProject/src/main/java/org/transitime/misc/GenerateMbtaBlockInfo.java
+++ mainProject/src/main/java/org/transitime/misc/GenerateMbtaBlockInfo.java
@@ -22,8 +22,10 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TimeZone;
 
 import org.slf4j.Logger;
@@ -51,6 +53,12 @@ public class GenerateMbtaBlockInfo {
 	// Keyed on tripId, value is blockId
 	private static Map<String, String> tripToBlockMap = 
 			new HashMap<String, String>();
+	
+	// For when a trip was assigned to multiple blocks.
+	// Keyed on the trip Id. Contains list of blocks associated
+	// with the trip.
+	private static Map<String, Set<String>> mismatchedAssignments = 
+			new HashMap<String, Set<String>>();
 	
 	private static final Logger logger = LoggerFactory
 			.getLogger(GenerateMbtaBlockInfo.class);
@@ -93,6 +101,37 @@ public class GenerateMbtaBlockInfo {
 	}
 	
 	/**
+	 * Didn't realize at beginning that assignments from the feed in the form
+	 * "dd00" need to be modified to be "dd" so that they match the trip short
+	 * names in the GTFS trips.txt file. This means that some early data had the
+	 * wrong trip identifier. To make this class work with the old problematic
+	 * names stored in the AVL database need to adjust them to remove the
+	 * trailing zeros.
+	 * 
+	 * @param assignment
+	 * @return The assignment adjust so that "dd00" becomes "dd"
+	 */
+	private static String adjustAssignment(String assignment) {
+		if (assignment.length() == 4 && assignment.endsWith("00"))
+			return assignment.substring(0,2);
+		else 
+			return assignment;
+	}
+	
+	/**
+	 * Returns the string padded to at least the desired length.
+	 * 
+	 * @param s
+	 * @param desiredLength
+	 * @return
+	 */
+	private static String pad(String s, int desiredLength) {
+		while (s.length() < desiredLength)
+			s += " ";
+		return s;
+	}
+	
+	/**
 	 * Processes days worth of AVL data to determine the block assignments
 	 * for each trip. Updates the static tripToBlockMap with the block 
 	 * assignment for each trip.
@@ -107,7 +146,7 @@ public class GenerateMbtaBlockInfo {
 				+ time.dateTimeStrMsecForTimezone(endDate.getTime()));
 		
 		// Get days worth of data from db
-		List<AvlReport> avlReports = AvlReport.getAvlReportsFromDb("mbta",
+		List<AvlReport> avlReports = AvlReport.getAvlReportsFromDb(
 				beginDate, endDate, null, "ORDER BY time");
 		System.out.println("Read in " + avlReports.size() 
 				+ " AVL reports for " 
@@ -142,8 +181,8 @@ public class GenerateMbtaBlockInfo {
 			String previousBlockId = "";
 			
 			for (AvlReport avlReport : avlReportsForVehicle) {
-				String tripShortName = avlReport.getAssignmentId();
-				String blockId = avlReport.getField1Value();
+				String tripShortName = adjustAssignment(avlReport.getAssignmentId());
+				String blockId = adjustAssignment(avlReport.getField1Value());
 				
 				// If no change in trip or block ID then don't need to output it
 				if (tripShortName.equals(previousTripShortName) && blockId.equals(previousBlockId))
@@ -157,10 +196,26 @@ public class GenerateMbtaBlockInfo {
 				Integer end = tripEndTimeMap.get(tripShortName);
 				String tripEndStr = end!=null ? Time.timeOfDayStr(end) : "--:--:--";
 				
+				String earlinessStr = "";
+				int earlinessOfAssignment = 0;
+				if (start != null) {
+					int avlTimeSecsInDay = time.getSecondsIntoDay(avlReport.getTime());
+					earlinessOfAssignment = start - avlTimeSecsInDay;
+					String s = Integer.toString(earlinessOfAssignment) + "s";
+					while (s.length() < 5)
+						s += " ";
+					earlinessStr = earlinessOfAssignment >= 0 ? 
+							s + " early" : s + " LATE!";
+				} else {
+					earlinessStr = "-----------";
+				}
+				
 				// Output the new trip & block info
 				System.out.println("  " 
 						+ " avlTime=" + time.dateTimeStrMsecForTimezone(avlReport.getTime())
-						+ " tripShortName(pattern)=" + tripShortName + " blockId(workpiece)=" + blockId 
+						+ " tripShortName(pattern)=" + pad(tripShortName, 4) 
+						+ " blockId(workpiece)=" + pad(blockId, 4)
+						+ " assigLeeway=" + earlinessStr
 						+ " tripStart=" + tripStartStr
 						+ " tripEnd=" + tripEndStr
 						+ " lat=" + avlReport.getLat() + " lon=" + avlReport.getLon());	
@@ -168,24 +223,48 @@ public class GenerateMbtaBlockInfo {
 				
 				time.getSecondsIntoDay(avlReport.getTime());
 				
-				// Add trip/block relationship to map. But only do so if not a special
+				// Should add trip/block relationship to map. But only do so if not a special
 				// trip 9999 or block 000.
 				if (tripShortName.equals("9999") || blockId.equals("000"))
 					continue;
+				
+				// If the assignment is really late then the block assignment 
+				// appears to be problematic. The block assignment seems to be 
+				// an early setting for the next assignment. In this case don't
+				// want to associate the block assignment with the trip. But
+				// sometimes get block "000" for the trip for a while and only
+				// then get a proper block assignment. For this case want to
+				// use the block assignment.
+				if (earlinessOfAssignment < 15 * Time.SEC_PER_MIN 
+						&& !previousBlockId.equals("000")) 
+					continue;
+				
+				// The assignment is OK so indeed add it to the map
 				String previousBlockForTrip = tripToBlockMap.get(tripShortName);
 				if (previousBlockForTrip != null 
 						&& !previousBlockForTrip.equals(blockId)) {
+					// Got mismatched assignment so keep track of such
+					Set<String> blocksForTrip = mismatchedAssignments.get(tripShortName);
+					if (blocksForTrip == null) {
+						blocksForTrip = new HashSet<String>();
+						mismatchedAssignments.put(tripShortName, blocksForTrip);						
+					}
+					// Add both the new and the old block ID to what is associated 
+					// with the trip. This way get the complete set.
+					blocksForTrip.add(blockId);
+					blocksForTrip.add(previousBlockForTrip);
+					
 					System.out.println("Got missmatch between block assignment "
 							+ "for tripId="	+ tripShortName 
 							+ ". Block for that trip was " + previousBlockForTrip 
 							+ " but for begin date " + beginDate + " it is " 
 							+ blockId);
-				} else {
-					// The block ID does not contradict a previous one for this 
-					// trip ID. Therefore remember this block assignment for 
-					// the trip.
-					tripToBlockMap.put(tripShortName, blockId);
-				}
+				} 
+				// Remember this block assignment for the trip.
+				// Note: sometimes temporarily get the wrong block. Therefore 
+				// need to use the last trip associated with a block. 
+				tripToBlockMap.put(tripShortName, blockId);
+			
 					
 			} // End of for each AVL report for the vehicle
 		} // End of for each vehicle
@@ -195,7 +274,7 @@ public class GenerateMbtaBlockInfo {
 	// Keyed on trip ID
 	private static Map<String, String> tripIdToTripShortNameMap = new HashMap<String, String>();
 	
-	// Keyed on trip short name
+	// Keyed on trip short name. Values are times in seconds into day
 	private static Map<String, Integer> tripStartTimeMap = new HashMap<String, Integer>();
 	private static Map<String, Integer> tripEndTimeMap = new HashMap<String, Integer>();
 	
@@ -252,6 +331,13 @@ public class GenerateMbtaBlockInfo {
 		for (int day=0; day < numberOfDays; ++day) {
 			Date date = new Date(beginDate.getTime() + day*Time.MS_PER_DAY);
 			processDayOfData(date);
+		}
+		
+		// Output the problematic blocks/trips
+		System.out.println("\nTrips associated with more than a single block:");
+		for (String tripShortName : mismatchedAssignments.keySet()) {
+			Set<String> blocksForTrip = mismatchedAssignments.get(tripShortName);
+			System.out.println("For tripShortName=" + tripShortName + " blocks=" + blocksForTrip);
 		}
 		
 		// Write out the supplemental trips file
