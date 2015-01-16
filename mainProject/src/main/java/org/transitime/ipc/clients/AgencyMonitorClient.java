dd93a7bca441cf5420894774499e8df0c4753bcd
Skibu Smith <skibu@smith.net>
2015-01-15 16:29:17 -0800

    Added ability for client to simply ping an agency using RMI so that can see if agency is running.

--- mainProject/src/main/java/org/transitime/ipc/clients/AgencyMonitorClient.java
+++ mainProject/src/main/java/org/transitime/ipc/clients/AgencyMonitorClient.java
@@ -18,13 +18,17 @@
 package org.transitime.ipc.clients;
 
 import java.rmi.RemoteException;
+import java.util.Collection;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.transitime.db.webstructs.WebAgency;
+import org.transitime.ipc.interfaces.ConfigInterface;
 import org.transitime.ipc.interfaces.ServerStatusInterface;
 
 /**
  * Makes the ServerStatusInterface.monitor() RMI call easy to access.
+ * Intended to be used on client, such as a web page on a web server.
  *
  * @author SkiBu Smith
  *
@@ -36,6 +40,54 @@ public class AgencyMonitorClient {
 
 	/********************** Member Functions **************************/
 
+	public static String pingAgency(String agencyId) {
+		String msg = null;
+
+		ConfigInterface configInterface = ConfigInterfaceFactory.get(agencyId);
+		if (configInterface == null) {
+			msg = "Could not create ConfigInterface for RMI";
+			logger.error(msg);
+		} else {
+			try {
+				// Do the RMI call to make sure agency is running and can
+				// communicate with it.
+				configInterface.getAgencies();
+			} catch (RemoteException e) {
+				msg = "Could not connect via RMI. " + e.getMessage();
+				logger.error(msg);
+			}
+		}
+		
+		return msg;
+	}
+	
+	/**
+	 * Does a simple ping to each agency to make sure that the core for that
+	 * agency is running and can communicate with it via IPC.
+	 * 
+	 * @return An error message if there is a problem, otherwise null
+	 */
+	public static String pingAllAgencies() {
+		String errorMessageForAllAgencies = "";
+		Collection<WebAgency> webAgencies = WebAgency.getWebAgencies();
+		for (WebAgency webAgency : webAgencies) {
+			if (webAgency.isActive()) {
+				// Actually do the low-level monitoring on the core system
+				String errorMessage = pingAgency(webAgency.getAgencyId());
+				if (errorMessage != null)
+					errorMessageForAllAgencies += "AgencyId "
+							+ webAgency.getAgencyId() + ": " + errorMessage 
+							+ "; ";
+			}
+		}
+		
+		// Return error message if there is one. Otherwise return null.
+		if (errorMessageForAllAgencies.length() > 0)
+			return errorMessageForAllAgencies;
+		else
+			return null;
+	}
+	
 	/**
 	 * Uses RMI to invoke monitoring of the agency core to see if everything is
 	 * operating properly. If there is a problem then and error message is
@@ -46,7 +98,7 @@ public class AgencyMonitorClient {
 	 * @return Error message if problem, or null
 	 */
 	public static String monitor(String agencyId) {
-	       ServerStatusInterface serverStatusInterface = 
+	    ServerStatusInterface serverStatusInterface = 
 	    		   ServerStatusInterfaceFactory.get(agencyId);
 	   	if (serverStatusInterface == null) {
 	   	    logger.error("Could not create ServerStatusInterface for RMI for "
@@ -59,9 +111,44 @@ public class AgencyMonitorClient {
 			resultStr = serverStatusInterface.monitor();
 		    return resultStr;
 		} catch (RemoteException e) {
-			logger.error("Exception when trying to monitor agencyId={}. {}", 
-					agencyId, e.getMessage());
-			return null;
+			WebAgency webAgency = WebAgency.getCachedWebAgency(agencyId);
+			logger.error("Exception when trying to monitor agency {}. {}", 
+					webAgency, e.getMessage());
+			return "Could not connect via RMI for " + webAgency;
 		}
 	}
+
+	/**
+	 * Goes through all agencies in the WebAgency database table that are
+	 * marked as active and monitor them to see if there are any problems.
+	 * The monitoring on the server side will automatically send out e-mail
+	 * notifications if there is a problems. This method sends out e-mails if
+	 * there is a problem connecting to an agency.
+	 * <p>
+	 * Probably not needed if using MonitoringModule which already monitors
+	 * an agency repeatedly.
+	 * 
+	 * @return
+	 */
+	public static String monitorAllAgencies() {
+		String errorMessageForAllAgencies = "";
+		Collection<WebAgency> webAgencies = WebAgency.getWebAgencies();
+		for (WebAgency webAgency : webAgencies) {
+			if (webAgency.isActive()) {
+				// Actually do the low-level monitoring on the core system
+				String errorMessage = monitor(webAgency.getAgencyId());
+				if (errorMessage != null)
+					errorMessageForAllAgencies += "AgencyId "
+							+ webAgency.getAgencyId() + ": " + errorMessage 
+							+ "; ";
+			}
+		}
+		
+		// Return error message if there is one. Otherwise return null.
+		if (errorMessageForAllAgencies.length() > 0)
+			return errorMessageForAllAgencies;
+		else
+			return null;
+	}
+	
 }
