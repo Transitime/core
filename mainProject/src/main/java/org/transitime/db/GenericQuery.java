ed96c3641117b2eee0c8d413d7ebd3a0237cec45
Skibu Smith <skibu@smith.net>
2015-01-30 13:55:07 -0800

    Reformatted file. No functional changes.

--- mainProject/src/main/java/org/transitime/db/GenericQuery.java
+++ mainProject/src/main/java/org/transitime/db/GenericQuery.java
@@ -40,113 +40,113 @@ import org.slf4j.LoggerFactory;
  */
 public class GenericQuery {
 
-    private static Connection connection;
+	private static Connection connection;
 
-    private static final Logger logger = LoggerFactory
-	    .getLogger(GenericQuery.class);
+	private static final Logger logger = LoggerFactory
+			.getLogger(GenericQuery.class);
 
-    public static class GenericResult {
-	// One long per column. Using a Long so that can handle longs
-	public List<Number> numbers;
-	public String text;
-    }
-
-    /********************** Member Functions **************************/
-
-    /**
-     * Constructor
-     * 
-     * @param dbType
-     * @param dbHost
-     * @param dbName
-     * @param dbUserName
-     * @param dbPassword
-     * @throws SQLException
-     */
-    public GenericQuery(String dbType, String dbHost, String dbName,
-	    String dbUserName, String dbPassword) throws SQLException {
-	connection = getConnection(dbType, dbHost, dbName, dbUserName,
-		dbPassword);
-    }
-
-    /**
-     * Gets a database connection to be used for the query
-     * 
-     * @param dbType
-     * @param dbHost
-     * @param dbName
-     * @param dbUserName
-     * @param dbPassword
-     * @return
-     * @throws SQLException
-     */
-    public static Connection getConnection(String dbType, String dbHost,
-	    String dbName, String dbUserName, String dbPassword)
-	    throws SQLException {
-	Connection conn = null;
-	Properties connectionProps = new Properties();
-	connectionProps.put("user", dbUserName);
-	connectionProps.put("password", dbPassword);
-
-	String url = "jdbc:" + dbType + "://" + dbHost + "/" + dbName;
-	conn = DriverManager.getConnection(url, connectionProps);
-	return conn;
-    }
-
-    /**
-     * Performs the specified generic query. A List of GenericResult objects is
-     * returned. All number columns (integer or float) are placed in
-     * GenericResult.numbers. A string column is assumed to be a tooltip and is
-     * put in GenericResult.text.
-     * 
-     * @param sql
-     * @return List of GenericResult. If no data then returns empty list
-     *         (instead of null)
-     * @throws SQLException
-     */
-    public List<GenericResult> doQuery(String sql) throws SQLException {
-	List<GenericResult> results = new ArrayList<GenericResult>();
-	Statement statement = null;
-
-	try {
-	    statement = connection.createStatement();
-	    ResultSet rs = statement.executeQuery(sql);
-	    ResultSetMetaData metaData = rs.getMetaData();
-	    // One result per row
-	    while (rs.next()) {
-		List<Number> numbers = new ArrayList<Number>();
-		String text = null;
-
-		for (int i = 1; i <= metaData.getColumnCount(); ++i) {
-		    int columnType = metaData.getColumnType(i);
-		    if (columnType == Types.INTEGER
-			    || columnType == Types.SMALLINT
-			    || columnType == Types.BIGINT) {
-			numbers.add(rs.getLong(i));
-		    } else if (columnType == Types.DOUBLE
-			    || columnType == Types.FLOAT) {
-			numbers.add(rs.getDouble(i));
-		    } else if (columnType == Types.VARCHAR) {
-			text = rs.getString(i);
-		    } else {
-			logger.warn("Encountered unknown result type {} "
-				+ "for columne {}", columnType, i);
-		    }
-		}
-
-		GenericResult result = new GenericResult();
-		result.numbers = numbers;
-		result.text = text;
-		results.add(result);
-	    }
-	} catch (SQLException e) {
-	    throw e;
-	} finally {
-	    if (statement != null)
-		statement.close();
+	public static class GenericResult {
+		// One long per column. Using a Long so that can handle longs
+		public List<Number> numbers;
+		public String text;
 	}
 
-	return results;
-    }
+	/********************** Member Functions **************************/
+
+	/**
+	 * Constructor
+	 * 
+	 * @param dbType
+	 * @param dbHost
+	 * @param dbName
+	 * @param dbUserName
+	 * @param dbPassword
+	 * @throws SQLException
+	 */
+	public GenericQuery(String dbType, String dbHost, String dbName,
+			String dbUserName, String dbPassword) throws SQLException {
+		connection = getConnection(dbType, dbHost, dbName, dbUserName,
+				dbPassword);
+	}
+
+	/**
+	 * Gets a database connection to be used for the query
+	 * 
+	 * @param dbType
+	 * @param dbHost
+	 * @param dbName
+	 * @param dbUserName
+	 * @param dbPassword
+	 * @return
+	 * @throws SQLException
+	 */
+	public static Connection getConnection(String dbType, String dbHost,
+			String dbName, String dbUserName, String dbPassword)
+			throws SQLException {
+		Connection conn = null;
+		Properties connectionProps = new Properties();
+		connectionProps.put("user", dbUserName);
+		connectionProps.put("password", dbPassword);
+
+		String url = "jdbc:" + dbType + "://" + dbHost + "/" + dbName;
+		conn = DriverManager.getConnection(url, connectionProps);
+		return conn;
+	}
+
+	/**
+	 * Performs the specified generic query. A List of GenericResult objects is
+	 * returned. All number columns (integer or float) are placed in
+	 * GenericResult.numbers. A string column is assumed to be a tooltip and is
+	 * put in GenericResult.text.
+	 * 
+	 * @param sql
+	 * @return List of GenericResult. If no data then returns empty list
+	 *         (instead of null)
+	 * @throws SQLException
+	 */
+	public List<GenericResult> doQuery(String sql) throws SQLException {
+		List<GenericResult> results = new ArrayList<GenericResult>();
+		Statement statement = null;
+
+		try {
+			statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery(sql);
+			ResultSetMetaData metaData = rs.getMetaData();
+			// One result per row
+			while (rs.next()) {
+				List<Number> numbers = new ArrayList<Number>();
+				String text = null;
+
+				for (int i = 1; i <= metaData.getColumnCount(); ++i) {
+					int columnType = metaData.getColumnType(i);
+					if (columnType == Types.INTEGER
+							|| columnType == Types.SMALLINT
+							|| columnType == Types.BIGINT) {
+						numbers.add(rs.getLong(i));
+					} else if (columnType == Types.DOUBLE
+							|| columnType == Types.FLOAT) {
+						numbers.add(rs.getDouble(i));
+					} else if (columnType == Types.VARCHAR) {
+						text = rs.getString(i);
+					} else {
+						logger.warn("Encountered unknown result type {} "
+								+ "for columne {}", columnType, i);
+					}
+				}
+
+				GenericResult result = new GenericResult();
+				result.numbers = numbers;
+				result.text = text;
+				results.add(result);
+			}
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (statement != null)
+				statement.close();
+		}
+
+		return results;
+	}
 
 }
