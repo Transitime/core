70e443621318167e274bc672b66047af54d1a399
Skibu Smith <skibu@smith.net>
2015-02-03 20:25:45 -0800

    Added main() so can easily create a WebAgency object in db.

--- mainProject/src/main/java/org/transitime/db/webstructs/WebAgency.java
+++ mainProject/src/main/java/org/transitime/db/webstructs/WebAgency.java
@@ -288,4 +288,36 @@ public class WebAgency {
 	public static Map<String, WebAgency> getCacheMap() {
 		return cacheMap;
 	}
+	
+	/**
+	 * For storing a web agency in the web database
+	 * 
+	 * @param args
+	 *            agencyId = args[0]; hostName = args[1]; dbType = args[2];
+	 *            dbHost = args[3]; dbUserName = args[4]; dbPassword = args[5];
+	 */
+	public static void main(String args[]) {
+		// Determine all the params
+		if (args.length <= 5) {
+			System.err.println("Specify params for the WebAgency: agencyId = args[0]; hostName = args[1]; dbType = args[2]; dbHost = args[3]; dbUserName = args[4]; dbPassword = args[5];");
+			System.exit(-1);
+		}
+		String agencyId = args[0];
+		String hostName = args[1];
+		boolean active = true;
+		String dbType = args[2];
+		String dbHost = args[3];
+		String dbUserName = args[4];
+		String dbPassword = args[5];
+		// Name of database where to store the WebAgency object
+		String dbName = "web";
+		
+		// Create the WebAgency object
+		WebAgency webAgency = new WebAgency(agencyId, hostName, active, dbType,
+				dbHost, dbUserName, dbPassword);
+		System.out.println("Storing " + webAgency);
+		
+		// Store the WebAgency
+		webAgency.store(dbName);
+	}
 }
