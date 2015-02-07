ecec2fe87a21ad2f9005e0b7fbab142f81087af5
Skibu Smith <skibu@smith.net>
2015-02-06 16:27:11 -0800

    Reformatted since having white space problem in Eclipse.

--- api/src/main/java/org/transitime/api/data/ApiBlock.java
+++ api/src/main/java/org/transitime/api/data/ApiBlock.java
@@ -35,56 +35,57 @@ import org.transitime.utils.Time;
  * @author SkiBu Smith
  *
  */
-@XmlRootElement(name="block")
+@XmlRootElement(name = "block")
 public class ApiBlock {
 
-    @XmlAttribute
-    private int configRev;
-    
-    @XmlAttribute
-    private String id;
-    
-    @XmlAttribute
-    private String serviceId;
+	@XmlAttribute
+	private int configRev;
 
-    @XmlAttribute
-    private String startTime; // In seconds from midnight
+	@XmlAttribute
+	private String id;
 
-    @XmlAttribute
-    private String endTime; // In seconds from midnight
+	@XmlAttribute
+	private String serviceId;
 
-    @XmlAttribute
-    private String headwaySecs;
+	@XmlAttribute
+	private String startTime; 
 
-    @XmlElement(name="trip")
-    private List<ApiTrip> trips;
+	@XmlAttribute
+	private String endTime; 
 
-    @XmlElement(name="route")
-    private List<ApiRouteSummary> routeSummaries;
+	@XmlAttribute
+	private String headwaySecs;
 
-    /********************** Member Functions **************************/
+	@XmlElement(name = "trip")
+	private List<ApiTrip> trips;
 
-    protected ApiBlock() {}
-    
-    public ApiBlock(IpcBlock ipcBlock) {
-	configRev = ipcBlock.getConfigRev();
-	id = ipcBlock.getId();
-	serviceId = ipcBlock.getServiceId();
-	startTime = Time.timeOfDayStr(ipcBlock.getStartTime());
-	endTime = Time.timeOfDayStr(ipcBlock.getEndTime());
-	headwaySecs = ipcBlock.getHeadwaySecs() != 0 ?
-		Integer.toString(ipcBlock.getHeadwaySecs()) : null;
-		
-	trips = new ArrayList<ApiTrip>();
-	for (IpcTrip ipcTrip : ipcBlock.getTrips()) {
-	    // Note: not including stop paths in trip pattern output
-	    // because that can bve really voluminous.
-	    trips.add(new ApiTrip(ipcTrip, false));
+	@XmlElement(name = "route")
+	private List<ApiRouteSummary> routeSummaries;
+
+	/********************** Member Functions **************************/
+
+	protected ApiBlock() {
 	}
-	
-	routeSummaries = new ArrayList<ApiRouteSummary>();
-	for (IpcRouteSummary ipcRouteSummary : ipcBlock.getRouteSummaries()) {
-	    routeSummaries.add(new ApiRouteSummary(ipcRouteSummary));
+
+	public ApiBlock(IpcBlock ipcBlock) {
+		configRev = ipcBlock.getConfigRev();
+		id = ipcBlock.getId();
+		serviceId = ipcBlock.getServiceId();
+		startTime = Time.timeOfDayStr(ipcBlock.getStartTime());
+		endTime = Time.timeOfDayStr(ipcBlock.getEndTime());
+		headwaySecs = ipcBlock.getHeadwaySecs() != 0 ? Integer
+				.toString(ipcBlock.getHeadwaySecs()) : null;
+
+		trips = new ArrayList<ApiTrip>();
+		for (IpcTrip ipcTrip : ipcBlock.getTrips()) {
+			// Note: not including stop paths in trip pattern output
+			// because that can bve really voluminous.
+			trips.add(new ApiTrip(ipcTrip, false));
+		}
+
+		routeSummaries = new ArrayList<ApiRouteSummary>();
+		for (IpcRouteSummary ipcRouteSummary : ipcBlock.getRouteSummaries()) {
+			routeSummaries.add(new ApiRouteSummary(ipcRouteSummary));
+		}
 	}
-    }
 }
