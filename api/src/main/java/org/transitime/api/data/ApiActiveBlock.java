e839f434bed864932904dbe53e7fe14d32917ce7
Skibu Smith <skibu@smith.net>
2015-02-10 15:23:51 -0800

    Added ability to get currently active blocks and associated vehicls from the API.

---
+++ api/src/main/java/org/transitime/api/data/ApiActiveBlock.java
@@ -0,0 +1,82 @@
+/*
+ * This file is part of Transitime.org
+ * 
+ * Transitime.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL) as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Transitime.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Transitime.org .  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.transitime.api.data;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+
+import org.transitime.ipc.data.IpcActiveBlock;
+import org.transitime.ipc.data.IpcTrip;
+import org.transitime.ipc.data.IpcVehicle;
+import org.transitime.utils.Time;
+
+/**
+ *
+ *
+ * @author SkiBu Smith
+ *
+ */
+public class ApiActiveBlock {
+
+	@XmlAttribute
+	private String id;
+
+	@XmlAttribute
+	private String serviceId;
+
+	@XmlAttribute
+	private String startTime; 
+
+	@XmlAttribute
+	private String endTime; 
+
+	@XmlElement(name = "trip")
+	private ApiTripSummary apiTripSummary;
+	
+	@XmlElement(name = "vehicles")
+	private ApiVehicles apiVehicles;
+	
+	/********************** Member Functions **************************/
+
+	protected ApiActiveBlock() {}
+	
+	public ApiActiveBlock(IpcActiveBlock ipcActiveBlock) {
+		id = ipcActiveBlock.getBlock().getId();
+		serviceId = ipcActiveBlock.getBlock().getServiceId();
+		startTime = Time.timeOfDayStr(ipcActiveBlock.getBlock().getStartTime());
+		endTime = Time.timeOfDayStr(ipcActiveBlock.getBlock().getEndTime());
+		
+		List<IpcTrip> trips = ipcActiveBlock.getBlock().getTrips();
+		IpcTrip ipcTrip = trips.get(ipcActiveBlock.getActiveTripIndex());
+		apiTripSummary = new ApiTripSummary(ipcTrip);
+		
+		Collection<IpcVehicle> vehicles = ipcActiveBlock.getVehicles();
+		apiVehicles = new ApiVehicles(vehicles);
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public ApiTripSummary getApiTripSummary() {
+		return apiTripSummary;
+	}
+}
