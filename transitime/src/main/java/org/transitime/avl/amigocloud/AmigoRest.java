fd204efddaef429c61e3e74a6ce45ca8f43283d3
skibu <skibu@smith.net>
2015-06-19 09:52:45 -0700

    Moved AmigoCloud AVL classes to under the avl package since they aren't really custom and want to be able to easily control logging of AVL stuff by having all AVl related classed be under a single directory.

--- transitime/src/main/java/org/transitime/avl/amigocloud/AmigoRest.java
+++ transitime/src/main/java/org/transitime/avl/amigocloud/AmigoRest.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Transitime.org .  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.transitime.custom.amigocloud;
+package org.transitime.avl.amigocloud;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
