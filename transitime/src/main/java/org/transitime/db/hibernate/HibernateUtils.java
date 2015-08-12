bd3b9648a0493786b9894f46e7f270084d71af33
skibu <skibu@smith.net>
2015-08-12 12:27:13 -0700

    Improved comments for getSession() methods to clearly indicate that the returned session always needs to be closed.

--- transitime/src/main/java/org/transitime/db/hibernate/HibernateUtils.java
+++ transitime/src/main/java/org/transitime/db/hibernate/HibernateUtils.java
@@ -201,11 +201,16 @@ public class HibernateUtils {
 	
 	/**
 	 * Returns session for the specified agencyId.
+	 * <p>
+	 * NOTE: Make sure you close the session after the query!! Use a try/catch
+	 * around the query and close the session in a finally block to make sure it
+	 * happens. The system only gets a limited number of sessions!!
 	 * 
 	 * @param agencyId
 	 *            Used as the database name if the property
 	 *            transitime.core.dbName is not set
-	 * @return The Session
+	 * @return The Session. Make sure you close it when done because system only
+	 *         gets limited number of open sessions.
 	 * @throws HibernateException
 	 */
 	public static Session getSession(String agencyId) 
@@ -219,8 +224,13 @@ public class HibernateUtils {
 	/**
 	 * Returns the session for the database name specified by the
 	 * transitime.db.dbName Java property.
+	 * <p>
+	 * NOTE: Make sure you close the session after the query!! Use a try/catch
+	 * around the query and close the session in a finally block to make sure it
+	 * happens. The system only gets a limited number of sessions!!
 	 * 
-	 * @return
+	 * @return The Session. Make sure you close it when done because system only
+	 *         gets limited number of open sessions.
 	 */
 	public static Session getSession() {
 		SessionFactory sessionFactory = 
