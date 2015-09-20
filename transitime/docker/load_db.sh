004216ae70c86371c75d59972f9d7fd5fbd263e0
Luqmaan <ldawoodjee@gmail.com>
2015-09-20 16:56:20 -0500

    Get things running locally

--- transitime/docker/load_db.sh
+++ transitime/docker/load_db.sh
@@ -1,2 +1,11 @@
-# Interestingly, the shell docker is running in doesn't like empty lines
-java -Dtransitime.db.dbName=cap-metro -Dtransitime.db.dbType=postgresql -Dtransitime.db.dbUsername=postgres -Dtransitime.db.dbPassword=transitime -Dtransitime.hibernate.configFile=hibernate.cfg.xml -Dhibernate.connection.url=jdbc:postgresql://$POSTGRES_PORT_5432_TCP_ADDR:$POSTGRES_PORT_5432_TCP_PORT/cap-metro -Dhibernate.connection.username=postgres -Dhibernate.connection.password=transitime -cp transitime.jar org.transitime.applications.GtfsFileProcessor -gtfsUrl "https://data.texas.gov/download/r4v4-vz24/application/zip"
+java \
+    -Dtransitime.db.dbName=cap-metro \
+    -Dtransitime.db.dbType=postgresql \
+    -Dtransitime.db.dbUsername=postgres \
+    -Dtransitime.db.dbPassword=transitime \
+    -Dtransitime.hibernate.configFile=hibernate.cfg.xml \
+    -Dhibernate.connection.url=jdbc:postgresql://$POSTGRES_PORT_5432_TCP_ADDR:$POSTGRES_PORT_5432_TCP_PORT/cap-metro \
+    -Dhibernate.connection.username=postgres \
+    -Dhibernate.connection.password=transitime \
+    -cp transitime.jar org.transitime.applications.GtfsFileProcessor \
+    -gtfsUrl "https://data.texas.gov/download/r4v4-vz24/application/zip"
--- transitime/docker/run_server.sh
+++ transitime/docker/run_server.sh
@@ -1,1 +1,9 @@
-java -Dtransitime.hibernate.configFile=hibernate.cfg.xml -Dhibernate.connection.url=jdbc:postgresql://$POSTGRES_PORT_5432_TCP_ADDR:$POSTGRES_PORT_5432_TCP_PORT/cap-metro -Dhibernate.connection.username=postgres -Dhibernate.connection.password=transitime -Dtransitime.avl.gtfsRealtimeFeedURI="https://data.texas.gov/download/i5qp-g5fd/application/octet-stream" -Dtransitime.modules.optionalModulesList=org.transitime.avl.GtfsRealtimeModule -Dtransitime.core.agencyId=cap-metro -cp transitime.jar org.transitime.applications.Core
+java \
+    -Dtransitime.hibernate.configFile=hibernate.cfg.xml \
+    -Dhibernate.connection.url=jdbc:postgresql://$POSTGRES_PORT_5432_TCP_ADDR:$POSTGRES_PORT_5432_TCP_PORT/cap-metro \
+    -Dhibernate.connection.username=postgres \
+    -Dhibernate.connection.password=transitime \
+    -Dtransitime.avl.gtfsRealtimeFeedURI="https://data.texas.gov/download/i5qp-g5fd/application/octet-stream" \
+    -Dtransitime.modules.optionalModulesList=org.transitime.avl.GtfsRealtimeModule \
+    -Dtransitime.core.agencyId=cap-metro \
+    -cp transitime.jar org.transitime.applications.Core
