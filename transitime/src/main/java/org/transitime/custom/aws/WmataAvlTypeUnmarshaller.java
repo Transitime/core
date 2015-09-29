37132aa4ff63f07cb2a802b01a5abd0ba088db23
dbenoff <dbenoff@camsys.com>
2015-09-29 14:06:12 -0700

    Fix unmarshalling of avlReports from SQS

--- transitime/src/main/java/org/transitime/custom/aws/WmataAvlTypeUnmarshaller.java
+++ transitime/src/main/java/org/transitime/custom/aws/WmataAvlTypeUnmarshaller.java
@@ -1,5 +1,6 @@
 package org.transitime.custom.aws;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.transitime.db.structs.AvlReport;
 import org.transitime.db.structs.AvlReport.AssignmentType;
@@ -12,57 +13,62 @@ import com.amazonaws.services.sqs.model.Message;
  */
 public class WmataAvlTypeUnmarshaller implements SqsMessageUnmarshaller {
 
-  @Override
-  public AvlReport toAvlReport(Message message) {
-    if (message == null || message.getBody() == null) return null;
-    
-    JSONObject jsonObj = new JSONObject(message.getBody());
-    JSONObject msgObj = new JSONObject(jsonObj.getString("Message")); 
-    
-    String vehicleId = null;
-    if (msgObj.has("vehicleid"))
-      vehicleId = String.valueOf(msgObj.getLong("vehicleid"));
-    Double lat = null;
-    if (msgObj.has("latitude"))
-      lat = msgObj.getDouble("latitude");
+    @Override
+    public AvlReport toAvlReport(Message message) {
+        if (message == null || message.getBody() == null) return null;
 
-    Double lon = null;
-    if (msgObj.has("longitude"))
-      lon = msgObj.getDouble("longitude");
+        JSONObject jsonObj = new JSONObject(message.getBody());
+        JSONObject msgObj;
+        try{
+            msgObj = new JSONObject(jsonObj.getString("Message"));
+        }catch(JSONException e){
+            msgObj = jsonObj;  //un
+        }
 
-    Long time = null;
-    if (msgObj.has("avlDate"))
-      time = msgObj.getLong("avlDate");
-    
-    Float heading = null;
-    if (msgObj.has("heading")) {  
-      heading = (float) msgObj.getDouble("heading");
+        String vehicleId = null;
+        if (msgObj.has("vehicleid"))
+            vehicleId = String.valueOf(msgObj.getLong("vehicleid"));
+        Double lat = null;
+        if (msgObj.has("latitude"))
+            lat = msgObj.getDouble("latitude");
+
+        Double lon = null;
+        if (msgObj.has("longitude"))
+            lon = msgObj.getDouble("longitude");
+
+        Long time = null;
+        if (msgObj.has("avlDate"))
+            time = msgObj.getLong("avlDate");
+
+        Float heading = null;
+        if (msgObj.has("heading")) {
+            heading = (float) msgObj.getDouble("heading");
+        }
+
+        Float speed = null;
+        if (msgObj.has("averageSpeed")) {
+            speed = (float) msgObj.getDouble("averageSpeed");
+        }
+
+        String source = "sqs";
+        if (vehicleId != null && lat != null && lon != null && time != null) {
+            AvlReport ar = new AvlReport(vehicleId, time, lat, lon, speed, heading, source);
+            if (msgObj.has("blockAlpha")) {
+                String blockAlpha = msgObj.getString("blockAlpha");
+                if (blockAlpha != null) {
+                    ar.setAssignment(blockAlpha, AssignmentType.BLOCK_ID);
+                }
+            }
+            return ar;
+        }
+        // missing necessary info
+        return null;
     }
-    
-    Float speed = null;
-    if (msgObj.has("averageSpeed")) {
-      speed = (float) msgObj.getDouble("averageSpeed");
+
+    @Override
+    public String toString(Message message) {
+        if (message == null) return null;
+        return message.getBody();
     }
-    
-    String source = "sqs";
-    if (vehicleId != null && lat != null && lon != null && time != null) {
-    	AvlReport ar = new AvlReport(vehicleId, time, lat, lon, speed, heading, source);
-    	if (msgObj.has("blockAlpha")) {
-    	  String blockAlpha = msgObj.getString("blockAlpha");
-    	  if (blockAlpha != null) {
-    	    ar.setAssignment(blockAlpha, AssignmentType.BLOCK_ID);
-    	  }
-    	}
-    	return ar;
-    }
-    // missing necessary info
-    return null;
-  }
-  
-  @Override
-  public String toString(Message message) {
-    if (message == null) return null;
-    return message.getBody();
-  }
 
 }
