c6b2d2767c03b3fe1c8082d18ae1c14904bd8197
skibu <skibu@smith.net>
2015-10-15 15:52:15 -0700

    Fixed MBTA AVL feed assignment ID issue to trim IDs that are 4-digits long and end with "00". This way will match the corresponding trip short names from the GTFS data.

--- transitime/src/main/java/org/transitime/custom/mbta/MbtaCommuterRailAvlModule.java
+++ transitime/src/main/java/org/transitime/custom/mbta/MbtaCommuterRailAvlModule.java
@@ -1,18 +1,18 @@
 /*
  * This file is part of Transitime.org
  * 
- * Transitime.org is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License (GPL) as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- *
- * Transitime.org is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Transitime.org .  If not, see <http://www.gnu.org/licenses/>.
+ * Transitime.org is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License (GPL) as published by the
+ * Free Software Foundation, either version 3 of the License, or any later
+ * version.
+ * 
+ * Transitime.org is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * Transitime.org . If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.transitime.custom.mbta;
@@ -36,8 +36,8 @@ import org.transitime.db.structs.AvlReport.AssignmentType;
 import org.transitime.modules.Module;
 
 /**
- * Gets GPS data from the MBTA Commuter Rail AVL feed. The data comes from a 
- * URL and is in TAIP format.
+ * Gets GPS data from the MBTA Commuter Rail AVL feed. The data comes from a URL
+ * and is in TAIP format.
  *
  * @author SkiBu Smith
  *
@@ -48,16 +48,17 @@ public class MbtaCommuterRailAvlModule extends PollUrlAvlModule {
 	// AVL reports to generate predictions and such then
 	// set shouldProcessAvl to false;
 	private static boolean shouldProcessAvl = true;
-	
+
 	// For logging use AvlModule class so that will end up in the AVL log file
 	private static final Logger logger = LoggerFactory
 			.getLogger(AvlModule.class);
 
 	/********************** Member Functions **************************/
-	
-	private static StringConfigValue mbtaCommuterRailFeedUrl = 
-			new StringConfigValue("transitime.avl.mbtaCommuterRailFeedUrl", 
+
+	private static StringConfigValue mbtaCommuterRailFeedUrl =
+			new StringConfigValue("transitime.avl.mbtaCommuterRailFeedUrl",
 					"The URL of the MBTA commuter rail feed to use.");
+
 	private static String getMbtaCommuterRailFeedUrl() {
 		return mbtaCommuterRailFeedUrl.getValue();
 	}
@@ -71,7 +72,9 @@ public class MbtaCommuterRailAvlModule extends PollUrlAvlModule {
 		super(agencyId);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.transitime.avl.AvlModule#getUrl()
 	 */
 	@Override
@@ -79,7 +82,9 @@ public class MbtaCommuterRailAvlModule extends PollUrlAvlModule {
 		return getMbtaCommuterRailFeedUrl();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.transitime.avl.AvlModule#processData(java.io.InputStream)
 	 */
 	@Override
@@ -89,7 +94,7 @@ public class MbtaCommuterRailAvlModule extends PollUrlAvlModule {
 		// order in the feed the element in the map represents the last
 		// AVL report.
 		Map<String, AvlReport> avlReports = new HashMap<String, AvlReport>();
-		
+
 		// Read in all AVL reports and add them to the avlReports map
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 		String line;
@@ -97,13 +102,13 @@ public class MbtaCommuterRailAvlModule extends PollUrlAvlModule {
 			// Log each non-blank line. Use debug logging since pretty verbose
 			if (!line.trim().isEmpty())
 				logger.debug(line);
-			
+
 			AvlReport avlReport = parseAvlReport(line);
 			if (avlReport != null) {
 				avlReports.put(avlReport.getVehicleId(), avlReport);
 			}
 		}
-		
+
 		// Process the last AVL report for each vehicle
 		if (shouldProcessAvl) {
 			for (AvlReport avlReport : avlReports.values()) {
@@ -111,49 +116,45 @@ public class MbtaCommuterRailAvlModule extends PollUrlAvlModule {
 			}
 		}
 	}
-	
+
 	/**
-	 * NOTE: used to be that the GTFS config always provided zero padded 3-digit
-	 * pattern/trip short names but the AVL feed would not include the preceding
-	 * zeros. Therefore had to adjust the assignment so that the non-padded
-	 * value from the feed would match the padded value from the config. But now
-	 * MBTA is supplying a supplemental trips.txt file that uses non-padded trip
-	 * short names. This means that don't need to adjust the assignments from
-	 * the feed anymore. Therefore this method simply returns the original
-	 * assignmentFromFeed.
+	 * The assignment IDs in the AVL feed don't always match the trip short
+	 * names in the GTFS data so sometimes the assignment IDs need to be
+	 * modified.
+	 * 
+	 * Previously, if an ID from the feed is only 1 or 2 characters long then it
+	 * needed to be padded with zeros so would end up with the 3-digit trip
+	 * short name in the GTFS data. But now MBTA is supplying a supplemental
+	 * trips.txt file that uses non-padded trip short names. This means that
+	 * don't need to adjust the short assignments from the feed anymore for
+	 * assignment IDs that are only 1 or 2 characters long.
 	 * <p>
-	 * Deprecated: Turns out that trip IDs in the AVL feed sometimes don't match
-	 * the trip short names in the trips.txt GTFS file. If an ID from the feed
-	 * is only 1 or 2 characters long then it needs to be padded with zeros. But
-	 * there is also another case where the IDs from the feed are 4 digits long
-	 * with two trailing zeroes. For these need to take of the trailing zeroes.
-	 * Specific examples of these are 61, 62, 63, 64, 67, 68, 69, 72, 94, 95,
-	 * 97, 98.
+	 * But there is also another case where the IDs from the feed are 4 digits
+	 * long with two trailing zeroes. For these need to take of the trailing
+	 * zeroes. Specific examples of these according to Dave Barker are
+	 * 51,52,53,54,55,56,57,58,61,62,63,64,65,66,67,68,69,72,94,95,97,98. But I
+	 * found that the 6x seem to no longer be used. To be safe simply modifying
+	 * any 4-digit ID that ends with a "00".
 	 * 
 	 * @param assignmentFromFeed
 	 * @return The assignment, adjusted to be proper number of characters so
 	 *         that matches trip short names in the trips.txt GTFS file
 	 */
 	private String adjustAssignment(String assignmentFromFeed) {
+		// Handle special null case
+		if (assignmentFromFeed == null)
+			return assignmentFromFeed;
+
+		// Trim assignment ID from feed if it is 4-digits long and ends 
+		// with "00".
+		if (assignmentFromFeed.length() == 4
+				&& assignmentFromFeed.endsWith("00"))
+			return assignmentFromFeed.substring(0, 2);
+
+		// Assignment is OK as is so return it
 		return assignmentFromFeed;
-//		
-//		// Handle special null case
-//		if (assignmentFromFeed == null)
-//			return assignmentFromFeed;
-//		
-//		// Assignment is too short so pad it
-//		if (assignmentFromFeed.length() == 1)
-//			return "00" + assignmentFromFeed;
-//		else if (assignmentFromFeed.length() == 2)
-//			return "0" + assignmentFromFeed;
-//		else if (assignmentFromFeed.length() == 4 
-//				&& assignmentFromFeed.endsWith("00"))
-//			return assignmentFromFeed.substring(0, 2);
-//		
-//		// Assignment is OK as is so return it
-//		return assignmentFromFeed;
 	}
-	
+
 	// The following are determining the proper place in the
 	// AVL string to process for the particular piece of data
 	private static String vehicleIdMarker = "Vehicle ID:";
@@ -161,7 +162,7 @@ public class MbtaCommuterRailAvlModule extends PollUrlAvlModule {
 	private static String workpieceMarker = "Workpiece:";
 	private static String patternMarker = "Pattern:";
 	private static String gpsMarker = "GPS:";
-	
+
 	/**
 	 * Reads line from AVL feed and creates and returns corresponding AVL
 	 * report.
@@ -177,23 +178,23 @@ public class MbtaCommuterRailAvlModule extends PollUrlAvlModule {
 			return null;
 		if (!line.contains(vehicleIdMarker) || !line.contains(locationMarker))
 			return null;
-		
+
 		// Get vehicle ID
 		String vehicleId = getValue(line, vehicleIdMarker + "(\\d+)");
-		
+
 		// Get the vehicle assignment. The "workpiece" is the block assignment
 		// and the "pattern" is the trip ID. Given that block assignments are
 		// a bit iffy for MBTA Commuter Rail it is best to use trip assignments.
-		String workpiece = 
+		String workpiece =
 				adjustAssignment(getValue(line, workpieceMarker + "(\\d+)"));
-		String pattern = 
+		String pattern =
 				adjustAssignment(getValue(line, patternMarker + "(\\d+)"));
-		
+
 		// Get GPS data from TAIP formatted string
 		String gpsTaipStr = getValue(line, gpsMarker + "(\\>.+\\<)");
 		TaipGpsLocation taipGpsLoc = TaipGpsLocation.get(gpsTaipStr);
 		if (taipGpsLoc == null) {
-			logger.debug("Could not parse TAIP string \"{}\" for line {}", 
+			logger.debug("Could not parse TAIP string \"{}\" for line {}",
 					gpsTaipStr, line);
 			return null;
 		}
@@ -202,11 +203,12 @@ public class MbtaCommuterRailAvlModule extends PollUrlAvlModule {
 		double lon = taipGpsLoc.getLongitude();
 		float heading = taipGpsLoc.getHeading();
 		float speed = taipGpsLoc.getSpeedMetersPerSecond();
-		
+
 		// Create the AVL report
-		AvlReport avlReport = new AvlReport(vehicleId, gpsTime, lat, lon,
-				speed, heading, "MBTA");
-		
+		AvlReport avlReport =
+				new AvlReport(vehicleId, gpsTime, lat, lon, speed, heading,
+						"MBTA");
+
 		// Determine the assignment to use. usingTripsAssignments should be
 		// true if using trips assignments instead regular block assignments.
 		// If using block assignments but the block is "000" then should
@@ -216,31 +218,30 @@ public class MbtaCommuterRailAvlModule extends PollUrlAvlModule {
 		// TODO need to determine whether going to use trip IDs or block IDs.
 		// Seems that could always use trip assignments since they are always
 		// provided (with blocks/workpieces sometimes get a "000" assignment).
-		// And with trip assignment the block assigner simply converts the 
+		// And with trip assignment the block assigner simply converts the
 		// trip to the block anyways. So only need the block/workpiece
 		// assignment to determine the trip to block mapping in order to
-		// generate the supplemental GTFS trips.txt file. Don't need 
+		// generate the supplemental GTFS trips.txt file. Don't need
 		// blocks/workpieces when actually running the core predictor software.
 		boolean usingTripAssignment = true;
-		
+
 		if (usingTripAssignment || workpiece.equals("000")) {
 			avlReport.setAssignment(pattern, AssignmentType.TRIP_SHORT_NAME);
 			avlReport.setField1("workpiece", workpiece);
 		} else {
-			// Using block assignment  that is not "000"
+			// Using block assignment that is not "000"
 			avlReport.setAssignment(workpiece, AssignmentType.BLOCK_ID);
 			avlReport.setField1("pattern", pattern);
 		}
-				
+
 		return avlReport;
 	}
-	
+
 	/**
-	 * Gets a value from the string using specified regular expression.
-	 * The regular expression should indicate what is before the value
-	 * plus a capture element to indicate the actual value. So for
-	 * a string containing something like Value:123 the regEx should
-	 * be "Value:{\\d+}".
+	 * Gets a value from the string using specified regular expression. The
+	 * regular expression should indicate what is before the value plus a
+	 * capture element to indicate the actual value. So for a string containing
+	 * something like Value:123 the regEx should be "Value:{\\d+}".
 	 * 
 	 * @param str
 	 * @param regEx
@@ -249,15 +250,15 @@ public class MbtaCommuterRailAvlModule extends PollUrlAvlModule {
 	private static String getValue(String str, String regEx) {
 		Pattern pattern = Pattern.compile(regEx);
 		Matcher matcher = pattern.matcher(str);
-		
+
 		boolean found = matcher.find();
 		if (!found)
 			return null;
-		
+
 		String result = str.substring(matcher.start(1), matcher.end(1));
 		return result;
 	}
-	
+
 	/**
 	 * Just for debugging
 	 */
@@ -265,10 +266,9 @@ public class MbtaCommuterRailAvlModule extends PollUrlAvlModule {
 		// For debugging turn off the actual processing of the AVL data.
 		// This way the AVL data is logged, but that is all.
 		shouldProcessAvl = false;
-		
+
 		// Create a NextBusAvlModue for testing
 		Module.start("org.transitime.custom.mbta.MbtaCommuterRailAvlModule");
 	}
 
 }
-
