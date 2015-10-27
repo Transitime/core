273fefe61dfd182125cfc890acaa1769f5581134
sheldonabrown <sheldonb@gmail.com>
2015-10-27 08:03:38 -0400

    Changing metric name to indicate percentage

--- transitime/src/main/java/org/transitime/monitoring/DatabaseQueueMonitor.java
+++ transitime/src/main/java/org/transitime/monitoring/DatabaseQueueMonitor.java
@@ -82,7 +82,7 @@ public class DatabaseQueueMonitor extends MonitorBase {
 				+ ".",
 				dbLogger.queueLevel());
 
-        cloudwatchService.publishMetric("DatabaseQueueLevel", dbLogger.queueLevel());
+        cloudwatchService.publishMetric("DatabaseQueuePercentageLevel", dbLogger.queueLevel());
 		
 		// Determine the threshold for triggering. If already triggered
 		// then lower the threshold by maxQueueFractionGap in order
