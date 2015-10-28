33544e877a1950a6c3a61ed487dd5b9704cc2c81
skibu <skibu@smith.net>
2015-10-28 08:37:21 -0700

    Changed transitime.monitoring.availableFreePhysicalMemoryThreshold default value to 10 MB to avoid getting unuseful e-mail messages.

--- transitime/src/main/java/org/transitime/monitoring/SystemMemoryMonitor.java
+++ transitime/src/main/java/org/transitime/monitoring/SystemMemoryMonitor.java
@@ -42,7 +42,7 @@ public class SystemMemoryMonitor extends MonitorBase {
 
 	LongConfigValue availableFreePhysicalMemoryThreshold = new LongConfigValue(
 			"transitime.monitoring.availableFreePhysicalMemoryThreshold", 
-			25 * 1024 * 1024L, // ~25 MB 
+			10 * 1024 * 1024L, // ~10 MB 
 			"If available free physical memory is less than this "
 			+ "value then free memory monitoring is triggered. This should be "
 			+ "relatively small since on Linux the operating system will use "
