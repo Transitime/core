68e4083e4bf4c41115e52e75a93ea3c5c63acee2
Brendan Egan <egan129129@gmail.com>
2016-06-08 11:28:17 +0100

    Changed package names and created Readme for QuickStart

---
+++ transitimeQuickStart/src/main/java/org/transitime/QuickStartGui/GuiMain.java
@@ -0,0 +1,41 @@
+
+
+
+
+
+package org.transitime.QuickStartGui;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+ 
+/* FrameDemo.java requires no other files. */
+public class GuiMain {
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("FrameDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(740,660));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
