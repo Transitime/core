0922e292a17564f67bb7e447a1d9191fd89541ad
Brendan Egan <egan129129@gmail.com>
2016-06-13 12:29:25 +0100

    moved Test for gtfsFileprocessor to transitime, alterd  TestGtfsFileProcessor

---
+++ transitime/src/test/java/org/transitime/config/TestGTFSfileprocessor.java
@@ -0,0 +1,45 @@
+package org.transitime.config;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+import org.transitime.applications.GtfsFileProcessor;
+import org.junit.Test;
+
+public class TestGTFSfileprocessor extends TestCase{
+
+	@Test
+	public void testProcess() {
+		fail("Not yet implemented");
+	}
+
+	
+		
+	        @Test
+			public void Testmain()
+			{
+	        	String configFile="C:\\Users\\Brendan\\Documents\\Transitime\\core\\transiTimeconfig.xml";
+	        	String notes=null;
+	        	String gtfsUrl=null;
+	        	String gtfsZipFileName=null;
+	        	String unzipSubdirectory=null;
+	        	String gtfsDirectoryName="C:\\Users\\Brendan\\Documents\\GTFS\\GTFSAUS\\";
+	        	String supplementDir=null;
+	        	String regexReplaceListFileName=null;
+	        	double pathOffsetDistance=0.0;
+	        	double maxStopToPathDistance=60.0;
+	        	double maxDistanceForEliminatingVertices=3.0;
+	        	int defaultWaitTimeAtStopMsec=10000;
+	        	double maxSpeedKph=97;
+	        	double maxTravelTimeSegmentLength=1000.0;
+	        	int configRev=-1;
+	        	boolean shouldStoreNewRevs=true;
+	        	boolean trimPathBeforeFirstStopOfTrip=false;
+			  String[] args ={"-c,C:\\Users\\Brendan\\Documents\\Transitime\\core\\transiTimeconfig.xml,-gtfsDirectoryName,C:\\Users\\Brendan\\Documents\\GTFS\\GTFSAUS\\,-storeNewRevs,-maxTravelTimeSegmentLength 1000"}; //for example
+			  GtfsFileProcessor Test = new GtfsFileProcessor(configFile,notes,gtfsUrl,gtfsZipFileName,unzipSubdirectory,gtfsDirectoryName,supplementDir,regexReplaceListFileName,pathOffsetDistance,maxStopToPathDistance,maxDistanceForEliminatingVertices,defaultWaitTimeAtStopMsec,maxSpeedKph,maxTravelTimeSegmentLength,configRev,shouldStoreNewRevs,trimPathBeforeFirstStopOfTrip);
+			  Assert();
+			}
+		    
+		
+	}
+
+
