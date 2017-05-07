89ff0630eaf6e5c47a35e14d150776a7e544cbe7
scrudden <og.crudden@gmail.com>
2017-05-07 13:08:40 +0100

    Get time from server and added alternative method to decide which vehicle to display.

--- transitimeWebapp/src/main/webapp/holding/js/transiTimeUtils.js
+++ transitimeWebapp/src/main/webapp/holding/js/transiTimeUtils.js
@@ -8,3 +8,22 @@ function unique(arr) {
     }
     return a;
 };
+function getCurrentServerTime(host, agencyId, processTime, data)
+{
+    console.log("Calling getCurrentServerTime.");
+
+    var baseurl = "http://"+host+"/api/v1/key/f78a2e9a/agency/"+agencyId+"/command/currentServerTime";
+
+    var xmlhttp = new XMLHttpRequest();
+                                
+    xmlhttp.onreadystatechange = function() {
+                   
+        if (this.readyState == 4 && this.status == 200) 
+        {                                              
+            var currentservertime = JSON.parse(this.responseText);
+            processTime(data, currentservertime);                                                                                       
+        }
+    };                                                             
+    xmlhttp.open("GET", baseurl, true);
+    xmlhttp.send();   
+};
