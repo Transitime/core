b1ee6b1fcc4c9e9aa6e729d51e592d8a40d0c5b6
vperez <vperez@192.168.100.126>
2018-07-10 21:22:51 -0400

    Cache API is described.
    The index page is changed to go to swagger documentation. This one has a
    link to previous one.

--- transitclockApi/src/main/java/org/transitclock/api/rootResources/TransitimeApi.java
+++ transitclockApi/src/main/java/org/transitclock/api/rootResources/TransitimeApi.java
@@ -117,7 +117,8 @@ import org.transitclock.ipc.interfaces.PredictionsInterface.RouteStop;
 description = "TheTransitClock is an open source transit information system."
 		+ " It’s core function is to provide and analyze arrival predictions for transit "
 		+ "systems.<br>Here you will find the detailed description of The Transit Clock API.<br>"
-		+ "For more information visit <a href=\"https://thetransitclock.github.io/\">thetransitclock.github.io.</a>"
+		+ "For more information visit <a href=\"https://thetransitclock.github.io/\">thetransitclock.github.io.</a><br> "
+		+ "The original documentation can be found in <a href=\"https://github.com/Transitime/core/wiki/API\">Api doc</a>."
 ),servers= {@Server(url="/api/v1")})
 @Path("/key/{key}/agency/{agency}")
 public class TransitimeApi {
