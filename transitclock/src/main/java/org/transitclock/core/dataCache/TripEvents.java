bb5d71ef4d5a81e76c8b652365d516b214e41698
scrudden <og.crudden@gmail.com>
2019-06-29 14:19:45 +0100

    Working on custom seralizer for cache.

--- transitclock/src/main/java/org/transitclock/core/dataCache/TripEvents.java
+++ transitclock/src/main/java/org/transitclock/core/dataCache/TripEvents.java
@@ -23,6 +23,31 @@ public class TripEvents implements Serializable {
 		Collections.sort(this.events, new IpcArrivalDepartureComparator());
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((events == null) ? 0 : events.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TripEvents other = (TripEvents) obj;
+		if (events == null) {
+			if (other.events != null)
+				return false;
+		} else if (!events.equals(other.events))
+			return false;
+		return true;
+	}
+
 	public TripEvents() {
 		super();		
 	}
