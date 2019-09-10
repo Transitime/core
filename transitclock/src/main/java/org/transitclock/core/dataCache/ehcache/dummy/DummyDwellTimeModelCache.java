6a32b22269a552aa3ba040cfebd55d834ad0692d
carabalb <lennycaraballo@gmail.com>
2019-09-10 17:22:00 -0400

    Adding dummy dwelltimemodelcache

---
+++ transitclock/src/main/java/org/transitclock/core/dataCache/ehcache/dummy/DummyDwellTimeModelCache.java
@@ -0,0 +1,30 @@
+package org.transitclock.core.dataCache.ehcache.dummy;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.transitclock.core.dataCache.DwellTimeModelCacheInterface;
+import org.transitclock.core.dataCache.StopPathCacheKey;
+import org.transitclock.db.structs.ArrivalDeparture;
+import org.transitclock.db.structs.Headway;
+
+public class DummyDwellTimeModelCache implements DwellTimeModelCacheInterface {
+
+    private static final Logger logger = LoggerFactory.getLogger(DummyDwellTimeModelCache.class);
+
+
+    @Override
+    public void addSample(ArrivalDeparture event, Headway headway, long dwellTime) {
+        return;
+    }
+
+    @Override
+    public void addSample(ArrivalDeparture departure) {
+        return;
+    }
+
+    @Override
+    public Long predictDwellTime(StopPathCacheKey cacheKey, Headway headway) {
+        return null;
+    }
+}
