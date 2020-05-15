54265e793458ad81fd0a64894e16a5db04cae2e7
sheldonabrown <sheldonb@gmail.com>
2020-05-15 17:17:10 -0400

    fixing class cast exception

--- transitclock/src/main/java/org/transitclock/core/dataCache/memcached/scheduled/KalmanErrorCache.java
+++ transitclock/src/main/java/org/transitclock/core/dataCache/memcached/scheduled/KalmanErrorCache.java
@@ -45,8 +45,9 @@ public class KalmanErrorCache implements ErrorCache {
 	@Override
 	public KalmanError getErrorValue(KalmanErrorCacheKey key) {
 
-		KalmanError value = (KalmanError) memcachedClient.get(createKey(key));
-		return value;
+		Double errorValue = (Double) memcachedClient.get(createKey(key));
+
+		return new KalmanError(errorValue);
 	}
 
 	@Override
