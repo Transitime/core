3ad83906ef08bf30663b7b8bcf613584924aaa42
carabalb <lennycaraballo@gmail.com>
2020-08-31 13:02:58 -0400

    Update TripRunTimeOutput

--- transitclockApi/src/main/java/org/transitclock/api/data/reporting/TripRunTimeOutput.java
+++ transitclockApi/src/main/java/org/transitclock/api/data/reporting/TripRunTimeOutput.java
@@ -1,5 +1,9 @@
+
+@XmlRootElement
 package org.transitclock.api.data.reporting;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.transitclock.api.data.reporting.chartjs.custom.TripRunTimeData;
 import org.transitclock.api.data.reporting.chartjs.custom.TripRunTimeMixedChart;
 import org.transitclock.ipc.data.IpcRunTimeForTrip;
@@ -15,19 +19,34 @@ import java.util.stream.Collectors;
 @XmlRootElement
 public class TripRunTimeOutput implements Serializable {
 
+    private static final Logger logger = LoggerFactory.getLogger(TripRunTimeOutput.class);
+
     public TripRunTimeOutput() {}
 
     public static TripRunTimeMixedChart  getAvgTripRunTimes(List<IpcRunTimeForTrip> runTimeForTrips){
         TripRunTimeData data = new TripRunTimeData();
         for(IpcRunTimeForTrip runTimeForTrip : sortRunTimes(runTimeForTrips)){
             data.getTripsList().add(getFormattedTripId(runTimeForTrip));
-            data.getFixedList().add(new BigDecimal(runTimeForTrip.getFixed()));
-            data.getVariableList().add(new BigDecimal(runTimeForTrip.getVariable()));
-            data.getDwellList().add(new BigDecimal(runTimeForTrip.getDwell()));
-            data.getScheduledList().add(new BigDecimal(runTimeForTrip.getScheduledTripCompletionTime()));
-            data.getNextTripStartList().add(new BigDecimal(runTimeForTrip.getNextScheduledTripStartTime()));
+            data.getFixedList().add(getValueAsBigDecimal(runTimeForTrip.getFixed()));
+            data.getVariableList().add(getValueAsBigDecimal(runTimeForTrip.getVariable()));
+            data.getDwellList().add(getValueAsBigDecimal(runTimeForTrip.getDwell()));
+            data.getScheduledList().add(getValueAsBigDecimal(runTimeForTrip.getScheduledTripCompletionTime()));
+            data.getNextTripStartList().add(getValueAsBigDecimal(runTimeForTrip.getNextScheduledTripStartTime()));
         }
         return new TripRunTimeMixedChart(data);
+    }
+
+    private static BigDecimal getValueAsBigDecimal(Double value){
+        if(value != null){
+            try{
+                return new BigDecimal(value).setScale(0,BigDecimal.ROUND_DOWN);
+            } catch (NumberFormatException nfe){
+                logger.warn("Unable to convert {} to a BigDecimal do to a number format exception",value, nfe);
+            } catch(Exception e){
+                logger.warn("Hit unexpected issue converting value {} to Big Decimal", value, e);
+            }
+        }
+        return new BigDecimal(0);
     }
 
     private static String getFormattedTripId(IpcRunTimeForTrip runTimeForTrip){
