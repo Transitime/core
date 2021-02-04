3fc8bdf442fe3915906f3b51d9d388e44e9d496b
sheldonabrown <sheldonb@gmail.com>
2021-02-04 07:29:05 -0500

    Adding LoadTrafficSensors application

---
+++ transitclock/src/main/java/org/transitclock/db/structs/TrafficPath.java
@@ -0,0 +1,121 @@
+/*
+ * This file is part of Transitime.org
+ *
+ * Transitime.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL) as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Transitime.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Transitime.org .  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.transitclock.db.structs;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+import org.hibernate.annotations.DynamicUpdate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.transitclock.db.hibernate.HibernateUtils;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderColumn;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Mirroring the conventions in StopPath, a TrafficPath is a set of points
+ * that defines the shape of a traffic sensor.  StopPaths are then mapped
+ * to these TrafficPaths where overlap occurs.
+ */
+@Entity(name="TrafficPaths")
+@DynamicUpdate
+@Table(name="TrafficPaths")
+public class TrafficPath implements Serializable {
+
+  @Column(length=2* HibernateUtils.DEFAULT_ID_SIZE)
+  @Id
+  private final String trafficPathId;
+
+  @Column
+  @Id
+  private final Integer trafficRev;
+
+  @ElementCollection(fetch=FetchType.LAZY)
+  @OrderColumn
+  private List<Location> locations;
+
+  @OneToMany(fetch=FetchType.LAZY)
+  @Cascade({CascadeType.SAVE_UPDATE})
+  @JoinTable(name="TrafficPath_to_StopPath_joinTable")
+  @OrderColumn( name="listIndex")
+  final protected List<StopPath> stopPaths;
+
+  @Column
+  private float pathLength;
+
+  private static long serialVersionUID = 1;
+
+  private static final Logger logger =
+          LoggerFactory.getLogger(TrafficPath.class);
+
+  /**
+   * Public constructor.
+   *
+   * @param trafficPathId
+   * @param trafficRev
+   * @param pathLength
+   */
+  public TrafficPath(String trafficPathId,
+                      int trafficRev,
+                      float pathLength) {
+    this.trafficPathId = trafficPathId;
+    this.trafficRev = trafficRev;
+    this.pathLength = pathLength;
+    stopPaths = new ArrayList<>();
+  }
+
+  /**
+   * Needed because Hibernate requires no-arg constructor
+   */
+  public TrafficPath() {
+    this.trafficPathId = null;
+    this.trafficRev = null;
+    this.pathLength = -1.0f;
+    stopPaths = new ArrayList<>();
+  }
+
+
+  public void setLocations(ArrayList<Location> locations) {
+    this.locations = locations;
+  }
+  public List<Location> getLocations() {
+    return locations;
+  }
+
+  public List<StopPath> getStopPaths() {
+    return stopPaths;
+  }
+
+  public String getTrafficPathId() {
+    return trafficPathId;
+  }
+
+  public int getTrafficRev() {
+    return trafficRev;
+  }
+
+}
---
+++ transitclock/src/main/java/org/transitclock/db/structs/TrafficSensor.java
@@ -0,0 +1,83 @@
+/*
+ * This file is part of Transitime.org
+ *
+ * Transitime.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL) as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Transitime.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Transitime.org .  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.transitclock.db.structs;
+
+import org.hibernate.annotations.DynamicUpdate;
+import org.transitclock.db.hibernate.HibernateUtils;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * Represents the sensor specific data from an external system.
+ */
+@Entity(name="TrafficSensor")
+@DynamicUpdate
+@Table(name="TrafficSensor")
+public class TrafficSensor implements Serializable {
+
+  @Column(length=HibernateUtils.DEFAULT_ID_SIZE)
+  @Id
+  private final String id;
+
+  @Column
+  @Id
+  private final Integer trafficRev;
+
+  @Column(length=HibernateUtils.DEFAULT_ID_SIZE)
+  private String externalId;
+
+  @Column(length=2*HibernateUtils.DEFAULT_ID_SIZE)
+  private String trafficPathId;
+
+  @Column
+  private String description;
+
+  /**
+   * Simple constructor
+   * @param id
+   * @param trafficRev
+   * @param externalId
+   */
+  public TrafficSensor(String id,
+                       int trafficRev,
+                       String externalId,
+                       String trafficPathId) {
+    this.id = id;
+    this.trafficRev = trafficRev;
+    this.externalId = externalId;
+    this.trafficPathId = trafficPathId;
+  }
+
+  /**
+   * Needed because Hibernate requires no-arg constructor
+   */
+  public TrafficSensor() {
+    this.id = null;
+    this.trafficRev = null;
+    this.externalId = null;
+    this.trafficPathId = null;
+  }
+
+  public void setDescription(String s) {
+    this.description = s;
+  }
+
+}
