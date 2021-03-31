8b2e4ae0a6c3c41fe291fd92996a88f8184c9447
carabalb <lennycaraballo@gmail.com>
2021-03-31 16:10:21 -0400

    Fix AvlJsonQuery to not have a dependency on headway info

--- transitclockWebapp/src/main/java/org/transitclock/reports/AvlJsonQuery.java
+++ transitclockWebapp/src/main/java/org/transitclock/reports/AvlJsonQuery.java
@@ -67,8 +67,7 @@ public class AvlJsonQuery {
 
 		String timeSql = getTimeSql(beginTime, endTime);
 		String headwayColSql = getHeadwayColSql(includeHeadway);
-		String headwayJoinSql = getHeadwayJoinSql(includeHeadway, beginDate);
-		String headwayWhereSql = getHeadwayWhereSql(includeHeadway);
+		String headwayJoinSql = getHeadwayJoinSql(includeHeadway);
 		String onTimePerformanceSql = getOnTimePerformanceSql(earlyMsec, lateMsec);
 
 		//need to limit the vehicle state table by time as well to utilize index on avlTime column
@@ -89,7 +88,6 @@ public class AvlJsonQuery {
 				+ headwayJoinSql
 				+ "WHERE a.time BETWEEN '" + beginDate + "' "
 				+ "AND TIMESTAMPADD(DAY,1,'" + beginDate + "') "
-				+ headwayWhereSql
 				+ timeSql;
 
 		// If only want data for single vehicle then specify so in SQL
@@ -157,21 +155,18 @@ public class AvlJsonQuery {
 		return headwayJoinSql;
 	}*/
 
-	private static String getHeadwayJoinSql(String includeHeadway, String beginDate){
+	private static String getHeadwayJoinSql(String includeHeadway){
 		String headwayJoinSql = "";
 		if	(includeHeadway != null && includeHeadway.equalsIgnoreCase("true")){
-			headwayJoinSql = "LEFT JOIN Headway h ON a.time= h.creationTime AND a.vehicleId = h.vehicleId ";
+			headwayJoinSql = "LEFT JOIN (" +
+					"SELECT creationTime, headway, vehicleId, scheduledHeadway " +
+					"FROM  Headway " +
+					"WHERE  headway < 10800000" +
+					") h " +
+					"ON a.time = h.creationTime " +
+					"AND a.vehicleId = h.vehicleId ";
 		}
 		return headwayJoinSql;
-	}
-
-	private static String getHeadwayWhereSql(String includeHeadway){
-		String sql = "";
-		if(includeHeadway != null && includeHeadway.equalsIgnoreCase("true")){
-			sql +=  "AND h.headway < " + MAX_HEADWAY;
-		}
-		return sql;
-
 	}
 
 	private static String getOnTimePerformanceSql(String early, String late){
