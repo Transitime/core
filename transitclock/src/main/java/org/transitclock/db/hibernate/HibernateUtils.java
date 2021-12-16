66d376862ac4599b1642767eb8bb24b6a1023b38
Jared Koester <jkoester@camsys.com>
2021-12-15 21:08:41 -0500

    Remove import

--- transitclock/src/main/java/org/transitclock/db/hibernate/HibernateUtils.java
+++ transitclock/src/main/java/org/transitclock/db/hibernate/HibernateUtils.java
@@ -32,7 +32,7 @@ import org.hibernate.service.ServiceRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.transitclock.configData.DbSetupConfig;
-import org.hibernate.service.ServiceRegistryBuilder;
+
 
 /**
  * Utilities for dealing with Hibernate issues such as sessions.
