eddc22cad2c31acdfc008f5b035ac62f2b04fe5d
Lenny C <lennycaraballo@gmail.com>
2022-06-13 10:07:11 -0400

    Additional methods to check for serviceType based on calendar type

--- transitclock/src/main/java/org/transitclock/core/ServiceTypeUtil.java
+++ transitclock/src/main/java/org/transitclock/core/ServiceTypeUtil.java
@@ -1,13 +1,16 @@
 package org.transitclock.core;
 
+import org.hibernate.Session;
+import org.transitclock.db.hibernate.HibernateUtils;
 import org.transitclock.db.structs.Calendar;
 import org.transitclock.db.structs.CalendarDate;
+import org.transitclock.utils.Time;
 
 import java.time.DayOfWeek;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.ZoneId;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class ServiceTypeUtil {
@@ -72,6 +75,29 @@ public class ServiceTypeUtil {
         return false;
     }
 
+    public static ServiceType getServiceTypeForCalendar(Calendar calendar){
+        if(calendar != null){
+            if(calendar.isOnWeekDay()){
+                return ServiceType.WEEKDAY;
+            } else if(calendar.getSaturday()){
+                return ServiceType.SATURDAY;
+            } else if(calendar.getSunday()){
+                return ServiceType.SUNDAY;
+            }
+        }
+        return null;
+    }
+
+    public static ServiceType getServiceTypeForCalendarDate(CalendarDate calendarDate){
+        if(calendarDate != null){
+            DayOfWeek dayOfWeek = Instant.ofEpochMilli(calendarDate.getDate().getTime())
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDate().getDayOfWeek();
+            return getServiceTypeForDay(dayOfWeek);
+        }
+        return null;
+    }
+
     public static boolean isCalendarDatesForServiceType(List<CalendarDate> calendarDates, ServiceType serviceType){
         if(calendarDates != null){
             for(CalendarDate calendarDate : calendarDates){
@@ -86,5 +112,46 @@ public class ServiceTypeUtil {
 
         }
         return false;
+    }
+
+    public static Map<String, Set<ServiceType>> getServiceTypesByIdForCalendars(int configRev,
+                                                                                LocalDate beginDate,
+                                                                                LocalDate endDate){
+        Map<String, Set<ServiceType>> serviceTypesByServiceId = new HashMap<>();
+        Session session = HibernateUtils.getSession(true);
+
+        Date calendarStartDate = Time.getLocalDateAsDate(beginDate);
+        Date calendarEndDate = Time.getLocalDateAsDate(endDate.plusDays(1));
+
+        // Process Calendars
+        List<Calendar> calendars = Calendar.getCalendars(session, configRev);
+        for(Calendar calendar : calendars){
+            Set<ServiceType> serviceTypes = serviceTypesByServiceId.get(calendar.getServiceId());
+            if(serviceTypes == null){
+                serviceTypes = new HashSet<>();
+                serviceTypesByServiceId.put(calendar.getServiceId(), serviceTypes);
+            }
+            ServiceType serviceType = ServiceTypeUtil.getServiceTypeForCalendar(calendar);
+            serviceTypes.add(serviceType);
+        }
+
+        // Process Calendar Dates
+
+        List<CalendarDate> calendarDates = CalendarDate.getCalendarDates(session, configRev);
+        for(CalendarDate calendarDate : calendarDates){
+            Set<ServiceType> serviceTypes = serviceTypesByServiceId.get(calendarDate.getServiceId());
+            // Only add if service id doesn't already exist
+            // We want to add unique service ids from Calendar Dates, not exceptions
+            if(serviceTypes == null
+                    && !calendarStartDate.after(calendarDate.getDate())
+                    && calendarDate.getDate().before(calendarEndDate)){
+                serviceTypes = new HashSet<>();
+                serviceTypesByServiceId.put(calendarDate.getServiceId(), serviceTypes);
+                ServiceType serviceType = ServiceTypeUtil.getServiceTypeForCalendarDate(calendarDate);
+                serviceTypes.add(serviceType);
+            }
+        }
+
+        return serviceTypesByServiceId;
     }
 }
