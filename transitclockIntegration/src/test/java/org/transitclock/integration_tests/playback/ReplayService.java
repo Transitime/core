be8edb6027ec4c8965f3e5f5e8de510a1bf1f8b7
sheldonabrown <sheldonb@gmail.com>
2023-07-12 06:29:12 -0400

    forcing timezone on integration tests

--- transitclockIntegration/src/test/java/org/transitclock/integration_tests/playback/ReplayService.java
+++ transitclockIntegration/src/test/java/org/transitclock/integration_tests/playback/ReplayService.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.transitclock.db.structs.ArrivalDeparture;
 import org.transitclock.integration_tests.prediction.TraceConfig;
+import org.transitclock.monitoring.MonitoringService;
 import org.transitclock.utils.DateRange;
 
 import java.util.*;
@@ -35,7 +36,9 @@ public class ReplayService {
         // Run trace
         logger.info(config.getDescription());
         System.out.println(config.getDescription());
+        MonitoringService.getInstance().flush();
         DateRange range = PlaybackModule.runTrace(config, null);
+        MonitoringService.getInstance().flush();
         return loader.queryArrivalDepartures(range, config.getArrivalDepartureCsv());
 
     }
